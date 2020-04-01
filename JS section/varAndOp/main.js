a2c9912b84dd39b02d64664e159073c53313b970
AmirBootbool <amir.bootbool@gmail,com>
2020-04-01 13:51:54 +0300

    conditionals exercise done

--- JS section/varAndOp/main.js
+++ JS section/varAndOp/main.js
@@ -1,11 +1,91 @@
-let a = 3
-a = 4
-let c = 0
-let b = a
-b = 2
-a = b
-b = c
-c = a
-a = b
+// code sandbox connected to HTML
 
-console.log(a , b, c)
+// let numChildren = parseInt(prompt("please type a number of childern 0 to 5 "))
+// const isCareful = false
+// if (isCareful === false){
+//     numChildren++
+// }
+// console.log(numChildren)
+// // refuses to run wuth const for the numC var
+
+// let silverware = 0
+// silverwareCount = parseInt(prompt("how maney silverware items do you see?"))
+// if (silverwareCount % 2 === 0){
+//     console.log("the count number is even")
+// } else {    console.log("the count number is odd- you had lost a fork?")
+// }
+
+// let performance = "stellar"
+// let salary = 10000
+// const goodBonus = 1000
+// const stellarBonus = 3000
+
+// if (performance === "stellar"){
+//     salary+=stellarBonus
+// } else {salary+=goodBonus}
+
+// console.log(salary)
+
+// const isVIP = false
+// let cash = 500
+
+// if (isVIP || cash > 300){
+//     console.log("Welcomem to De-Club Amigo mio")
+// } else{
+//     console.log("sorry dude -its a Ladies night")
+// }
+
+// const gender = null
+// const gender = "male"
+// const gender = "female"
+
+// let profession = "business"
+
+
+// if (gender === "male"){
+//     console.log(profession+="man")
+// } else if (gender === "female"){
+//     console.log(profession+="woman")
+// } else if (gender=== null){
+//     console.log(profession+="person")
+// }
+
+
+
+// let boughtTesla = false
+// let boughtTesla = true
+
+// let isUSCitizen = true
+// let isUSCitizen = false
+
+// if (boughtTesla){
+//     if (isUSCitizen){
+//         let years = parseInt(prompt("how many years had passed since you got it?"))
+//         if (years >= 4){
+//             console.log("would like an upgrade Madame?")
+//         } else {
+//             console.log("are you satisfied so far from your purchase?")
+//         }
+//     } else {
+//         console.log(" would like to move to new-york? we have planty of free Corona")
+//     }
+// } else {
+//     console.log("would you like to buy a new Tesla Madam? ")
+// }
+
+// const a = 3
+// let b = 2
+// let c = 0 || 12
+// let d
+
+// b = c
+// b++
+// if(d){
+//   b = a
+// }
+
+// d = a + (b * c)
+// d++
+// b += 2
+
+// console.log(a,b,c,d) 
